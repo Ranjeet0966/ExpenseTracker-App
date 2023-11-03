7163744d2134b529a6637d23adada469ae84e984
Ranjeet Kumar Yadav <innovativeranjeet@gmail.com>
2023-11-03 10:16:05 -0400

    asignment no 7

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,7 @@ exports.authenticate = async (req, res, next) => {
   try {
     const token = req.header("Authorization");
     const { userId } = jwt.verify(token, "1234567890");
+    // print user is
     console.log('userID >>>>', userId);
     const user = await SignUp.findByPk(userId);
     req.user = user;
